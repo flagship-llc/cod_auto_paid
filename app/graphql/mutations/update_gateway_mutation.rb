c7a0915eb7fb5802299bc18ed455aa31d9431568
djjimba <dj.jimba@gmail.com>
2018-06-10 19:35:05 +0900

    removes double initialization

--- app/graphql/mutations/update_gateway_mutation.rb
+++ app/graphql/mutations/update_gateway_mutation.rb
@@ -1,21 +1,19 @@
-class Mutations::UpdateGatewayMutation < GraphQL::Schema::RelayClassicMutation
-  Mutations::UpdateGatewayMutation = GraphQL::Relay::Mutation.define do
-	  name 'UpdateGateway'
+Mutations::UpdateGatewayMutation = GraphQL::Relay::Mutation.define do
+  name 'UpdateGateway'
 
-	  input_field :gateway_name, !types.String
-	  return_field :shop, Types::ShopType
+  input_field :gateway_name, !types.String
+  return_field :shop, Types::ShopType
 
-	  resolve ->(_obj, inputs, ctx) {
-	    begin
-	      shop = Shop.find(ctx[:current_shop].id)
-	      shop.gateway_name = inputs.gateway_name
-	      shop.save
-	    rescue => e
-	      return GraphQL::ExecutionError.new(e.message)
-	    end
+  resolve ->(_obj, inputs, ctx) {
+    begin
+      shop = Shop.find(ctx[:current_shop].id)
+      shop.gateway_name = inputs.gateway_name
+      shop.save
+    rescue => e
+      return GraphQL::ExecutionError.new(e.message)
+    end
 
-	    { shop: shop }
-	  }
+    { shop: shop }
+  }
 
-	end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,16 +1,10 @@
-class Types::QueryType < Types::BaseObject
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
+Types::QueryType = GraphQL::ObjectType.define do
+  name "Query"
 
-  Types::QueryType = GraphQL::ObjectType.define do
-  	name "Query"
-
-	field :shop do
-		type Types::ShopType
-		resolve ->(obj, args, ctx) {
-		  Shop.find(ctx[:current_shop].id)
-		}
-	end
+  field :shop do
+	type Types::ShopType
+	resolve ->(obj, args, ctx) {
+	  Shop.find(ctx[:current_shop].id)
+	}
   end
-
 end
