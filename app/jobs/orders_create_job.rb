40b6d16e6f8cbe9b06e99ef3762f689d764230b6
Sean Kamioka <seannik@bluewin.ch>
2019-12-16 17:41:16 +0900

    payment status of pending removed from validation

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -5,7 +5,7 @@ class OrdersCreateJob < ActiveJob::Base
     shop = Shop.find_by(shopify_domain: shop_domain)
     shop.with_shopify_session do
     	puts webhook['gateway']
-    	if webhook['financial_status'] == "pending" and webhook['gateway'] == "Cash on Delivery (COD)" || webhook['gateway'].include?('代金')
+    	if webhook['gateway'] == "Cash on Delivery (COD)" || webhook['gateway'].include?('代金')
 		    ShopifyAPI::Transaction.create({order_id: webhook['id'], kind: 'capture'})
     	end
     end
