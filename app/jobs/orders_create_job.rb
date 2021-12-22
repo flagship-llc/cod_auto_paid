6ecc80a4a5666ec9dcc42e221fec41a0d7525d61
sondh5 <hongson.do@flagship.cc>
2021-12-22 12:24:30 +0900

    Handle parent information when capture transaction

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -4,10 +4,21 @@ class OrdersCreateJob < ActiveJob::Base
   def perform(shop_domain:, webhook:)
     shop = Shop.find_by(shopify_domain: shop_domain)
     shop.with_shopify_session do
-    	puts webhook['gateway']
-    	if webhook['gateway'] == "Cash on Delivery (COD)" || webhook['gateway'].include?('代金')
-		    ShopifyAPI::Transaction.create({order_id: webhook['id'], kind: 'capture'})
-    	end
+      puts webhook['gateway']
+      return unless webhook['gateway'] == "Cash on Delivery (COD)" || webhook['gateway'].include?('代金')
+
+      order_id = webhook['id']
+      shopify_order_transactions = ShopifyAPI::Transaction.find(:all, params: { order_id: order_id })&.to_a
+      shopify_parent_transaction = (shopify_order_transactions || []).select { |t| t.kind == 'authorization' }.last
+
+      capture_transaction_record = {
+      kind: 'capture',
+      order_id: order_id,
+      parent_id: shopify_parent_transaction&.id,
+      authorization: shopify_parent_transaction&.authorization
+      }
+
+      ShopifyAPI::Transaction.create(capture_transaction_record)
     end
   end
 end
